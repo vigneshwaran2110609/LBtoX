65775e3ab0eb673fac66b1a5863568f31587c3ac
vigneshwaran2110609 <107832637+vigneshwaran2110609@users.noreply.github.com>
2025-09-14 22:51:55 +0530

    lock on processing checkpoint

--- src/main/java/com/example/LBtoX/repositories/ProcessingCheckpointRepository.java
+++ src/main/java/com/example/LBtoX/repositories/ProcessingCheckpointRepository.java
@@ -3,12 +3,23 @@ package com.example.LBtoX.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.repository.query.Param;
+import jakarta.persistence.LockModeType;
 import com.example.LBtoX.models.*;
 
 @Repository
 public interface ProcessingCheckpointRepository extends JpaRepository<ProcessingCheckpoint, Long>{
 	
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	@Query("SELECT p FROM ProcessingCheckpoint p")
     ProcessingCheckpoint findCheckpoint();
+	
+	@Modifying
+	@Query("UPDATE ProcessingCheckpoint p SET p.lastProcessedId = :newValue")
+	int updateLastProcessedId(@Param("newValue") Long newValue);
+	
+	
 
 }
